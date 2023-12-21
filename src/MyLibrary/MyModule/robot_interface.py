a8f9a9a18a3a37deb81b5fadf0e433bd5f82c3f9
myjniak <myjniak@gmail.com>
2023-12-21 14:15:14 +0100

    :<

---
+++ src/MyLibrary/MyModule/robot_interface.py
@@ -0,0 +1,8 @@
+from robot.api import logger
+
+
+class MyModule:
+
+    @staticmethod
+    def my_super_keyword():
+        logger.console("I'm working, yay ^_^")
---
+++ test/test.robot
@@ -0,0 +1,6 @@
+*** Settings ***
+Library    MyLibrary.MyModule    AS    MyModule
+
+*** Test Cases ***
+Test 1
+    MyModule.My Super Keyword
